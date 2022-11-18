25fafe10beb4d9aa8647d931ab7807b8dbbce5e6
John Wohler <johndoubleyou11@gmail.com>
2022-11-18 16:36:05 -0400

    Finished

---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TriggerScript : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    public bool OneTimeTrigger = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Trigger1");
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Trigger1");
+
+            timeline.Play();
+
+            if (OneTimeTrigger == true)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
+    }
+
+
+
+
+}
